5ea04fe2fedc5befc3407ca9e652c5c81381153c
Emmanuel <emmanuel.arana.gutierrez@gmail.com>
2024-12-01 21:51:22 -0600

    fix rutas projects json assets public

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,37 +1,3 @@
-import CalidadSEMS from "../assets/projects/Calidad SEMS Credits.png";
-
-
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
-
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-
-export const EXPERIENCES = [
-  {
-    year: "2022 - Present",
-    role: "Full Stack Developer",
-    company: "SEMS UDG",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["JavaScript", "React.JS", "Express.JS", "Sequelize", "mySQL", "Material-UI", "Bootstrap"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Social Service Worker",
-    company: "SEMS UDG",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["JavaScript", "React.JS", "HTML", "CSS", "mySQL", "Bootstrap"],
-  },
-];
-
-export const PROJECTS = [
-  {
-    title: "Calidad SEMS",
-    image: CalidadSEMS,
-    description:
-      "A fully functional web app made for the UDG school network. https://calidad.sems.udg.mx/Creditos",
-    technologies: ["JavaScript", "React.JS", "Express.JS", "Sequelize", "mySQL", "Material-UI", "Bootstrap"],
-  }
-];
-
 export const CONTACT = {
   address: "Guadalajara Jálisco, México",
   phoneNo: "+52 33-1041-9236 ",
